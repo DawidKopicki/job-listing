4a595d4bd6a11d0ac96a06c30fb7d4c9d70ba7df
DawidKopicki <dawidkopicki@gmail.com>
2020-05-27 12:15:53 +0200

    Implementing data filtering

--- script.js
+++ script.js
@@ -149,32 +149,75 @@ const jobsData = [{
         "tools": ["React", "Sass"]
     }
 ];
+const filteredArgs = [];
 
-const htmlOfferTemplate = jobsData.map(el => {
+const displayOffers = (data = jobsData) => {
+    if (document.querySelector('.site-main-section')) document.querySelector('.site-main').innerHTML = '';
+    console.log(document.querySelector('.site-main'));
+    data.map(el => {
+        const jobOffer = `
+        <header class="site-main-section__header">
+            <img src="${el.logo}" alt="${el.company}" class="site-main-section__image">
+            <div class="site-main-section__header-info">
+                <h2 class="site-main-section__header-info-company">${el.company}</h2>
+                ${el.new ? '<button class="site-main-section__header-info-badge">New!</button>' : ''}
+                ${el.featured ? '<button class="site-main-section__header-info-badge">Featured</button>' : ''}
+            </div>
+            <h2 class="site-main-section__header-role">${el.position}</h2>
+            <div class="site-main-section__header-list">
+                <span>${el.postedAt}</span>
+                <span>&bull; ${el.contract}</span>
+                <span>&bull; ${el.location}</span>
+            </div>
+        </header>
+        <aside>
+            ${el.languages.map(lang => `<button class="site-main-section__button">${lang}</button>`).join('')}
+            ${el.tools.map(tool => `<button class="site-main-section__button">${tool}</button>`).join('')}
+        </aside>
+    `
+        const offerSection = document.createElement('section');
+        offerSection.className = `site-main-section ${el.featured ? 'u-border-featured' : ''}`;
+        offerSection.innerHTML = jobOffer;
 
-    const jobOffer = `
-    <header class="site-main-section__header">
-        <img src="${el.logo}" alt="${el.company}" class="site-main-section__image">
-        <div class="site-main-section__header-info">
-            <h2 class="site-main-section__header-info-company">${el.company}</h2>
-            ${el.new ? '<button class="site-main-section__header-info-badge">New!</button>' : ''}
-            ${el.featured ? '<button class="site-main-section__header-info-badge">Featured</button>' : ''}
-        </div>
-        <h2 class="site-main-section__header-role">${el.position}</h2>
-        <div class="site-main-section__header-list">
-            <span>${el.postedAt}</span>
-            <span>&bull; ${el.contract}</span>
-            <span>&bull; ${el.location}</span>
-        </div>
-    </header>
-    <aside>
-        ${el.languages.map(lang => `<button>${lang}</button>`).join('')}
-        ${el.tools.map(tool => `<button>${tool}</button>`).join('')}
-    </aside>
-`
-    const offerSection = document.createElement('section');
-    offerSection.className = `site-main-section ${el.featured ? 'u-border-featured' : ''}`;
-    offerSection.innerHTML = jobOffer;
+        document.querySelector('.site-main').appendChild(offerSection);
 
-    document.querySelector('.site-main').appendChild(offerSection);
-});
+    });
+};
+
+const addToFiltering = e => {
+    filteredArgs.push(e.target.innerText);
+
+    filterOffers(filteredArgs);
+};
+
+const filterOffers = filteredArgs => {
+    const header = document.querySelector('.site-header');
+    const aside = document.createElement('aside');
+    aside.className = 'site-header-filter';
+
+    filteredArgs.forEach(el => {
+        const button = document.createElement('button');
+        button.textContent = el;
+        const deleteElement = document.createElement('span')
+        deleteElement.textContent = 'X';
+        button.appendChild(deleteElement);
+        aside.appendChild(button)
+    });
+
+    header.appendChild(aside);
+
+    const filteredData = jobsData.filter(el => el.languages.includes(...filteredArgs) || el.tools.includes(...filteredArgs));
+
+    rerenderOffers(filteredData)
+};
+
+const rerenderOffers = (filteredData) => {
+    const section = document.querySelector('.site-main-section').remove();
+
+    displayOffers(filteredData);
+}
+
+displayOffers();
+
+const buttons = document.querySelectorAll('.site-main-section__button');
+buttons.forEach(button => button.addEventListener('click', addToFiltering));
